71247d26ec14bde817a4920a582c7d902543b047
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-12 16:05:08 +0530

    Added: UC1_CreateBST

---
+++ Day-15-BinarySerchTree_and_HashTable/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_15_BinarySerchTree_and_HashTable;
+
+Console.WriteLine("----- Binary_Search_Tree -----\n");
+
+BinarySearchTreeOps<int> binarySearchTree = new BinarySearchTreeOps<int>(56); //Root=56  
+binarySearchTree.Insert(30);
+binarySearchTree.Insert(70);
+
+Console.WriteLine("\n>> Binary_Search_Tree :- ");
+binarySearchTree.Display();
+
+Console.ReadKey();
