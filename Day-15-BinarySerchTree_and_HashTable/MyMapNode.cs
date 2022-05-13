95514d61c66e38323e57936dcd71dd072d5c0554
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-13 23:21:20 +0530

    Added: UC2_FindFreqofParagraph

--- Day-15-BinarySerchTree_and_HashTable/MyMapNode.cs
+++ Day-15-BinarySerchTree_and_HashTable/MyMapNode.cs
@@ -8,21 +8,21 @@ namespace Day_15_BinarySerchTree_and_HashTable
 {
     internal class MyMapNode<K, V>
     {
-        private readonly int size;
-        private readonly LinkedList<KeyValue<K, V>>[] items;
+        private readonly int size; //declaring size variable of readonly type
+        private readonly LinkedList<KeyValue<K, V>>[] items; //declaring referance array variable of generic type
         public MyMapNode(int size)
-        {
-            this.size = size;
+        { //intitaiallising the instances of current class
+            this.size = size; 
             this.items = new LinkedList<KeyValue<K, V>>[size];
         }
-        public void Add(K key, V value)
+        public void Add(K key, V value)  //this methd add <key,value> that we have passed while calling the methd
         {
-            int position = GetArrayPosition(key);
-            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
-            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
-            linkedList.AddLast(item);
+            int position = GetArrayPosition(key); //here getArrayPostion methd is called tp get HashCode of String Key
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position); // GetLinkedList methd is called to create LinkedList of respective Position
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };//Object of Strcture KeyValue is getting created
+            linkedList.AddLast(item);//Adding item in LinkedList using inbulit methd AddLast
         }
-        public void Remove(K key)
+        public void Remove(K key)  //Used to remove KeyValue pair
         {
             int position = GetArrayPosition(key);
             LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
@@ -43,27 +43,14 @@ namespace Day_15_BinarySerchTree_and_HashTable
 
 
         }
-        public V Get(K key)
-        {
-            int position = GetArrayPosition(key);
-            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
-            foreach (KeyValue<K, V> item in linkedList)
-            {
-                if (item.Key.Equals(key))
-                {
-                    return item.Value;
-                }
-            }
-            return default(V);
-        }
 
-        protected int GetArrayPosition(K key)
+        protected int GetArrayPosition(K key) //used to get the hash code os string Key
         {
             int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
 
-        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position) //used to create LinkedList at Position of items array
         {
             LinkedList<KeyValue<K, V>> linkedList = items[position];
             if (linkedList == null)
@@ -74,14 +61,14 @@ namespace Day_15_BinarySerchTree_and_HashTable
             return linkedList;
         }
 
-        public struct KeyValue<k, v>
+        public struct KeyValue<k, v>  //Structure to get Structure of <Key,Value>
         {
             public k Key { get; set; }
             public v Value { get; set; }
         }
 
 
-        public int CheckHash(K key)
+        public int CheckHash(K key) //returns count of which the KeyValue is repeated in given array of string
         {
             int position = GetArrayPosition(key);
             LinkedList<KeyValue<K, V>> LinkedListofPosition = GetLinkedList(position);
@@ -110,10 +97,11 @@ namespace Day_15_BinarySerchTree_and_HashTable
 
         }
 
-        public void GetFreq()
+        public void GetFreq() // get the Freq of Repetation of Strings in our items array of size 16
         {
-            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(6);
-            string[] words = { "to", "be", "or", "not", "to", "be" };
+            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(19);
+            string[] words = { "Paranoids","are","not","paranoid","because","they","are","paranoid","but","because","they","keep","putting","themselves",
+                               "deliberately","into","paranoid","avoidable","situations" };
             int count = 1;
             foreach (string i in words)
             {
@@ -136,7 +124,7 @@ namespace Day_15_BinarySerchTree_and_HashTable
         }
         
 
-    public void Display(K key)
+    public void Display(K key) //Extract all KeyValue pairs and display on console
         {
             int position = GetArrayPosition(key);
             LinkedList<KeyValue<K, V>> LinkedListofPosition = GetLinkedList(position);
@@ -144,7 +132,7 @@ namespace Day_15_BinarySerchTree_and_HashTable
             {
                 if (keyValue.Key.Equals(key))
                 {
-                    Console.WriteLine("{ KeyValue : " + keyValue.Key + " , Freq of Occurance : " + keyValue.Value+" }");
+                    Console.WriteLine("[ > KeyValue : " + keyValue.Key + "    => Freq of Occurance : " + keyValue.Value+" ]\n");
                 }
 
             }
